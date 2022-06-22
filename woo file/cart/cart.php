9a42b799da0a2263dfd198826236e8f9d9031122
nayonseopage1 <nayon@seopage1.net>
2022-06-22 12:42:56 +0600

    update file
    Signed-off-by: nayonseopage1 <nayon@seopage1.net>

---
+++ woo file/cart/cart.php
@@ -0,0 +1,140 @@
+<?php
+/**
+ * Cart Page
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/cart/cart.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see     https://docs.woocommerce.com/document/template-structure/
+ * @package WooCommerce\Templates
+ * @version 3.8.0
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+do_action( 'woocommerce_before_cart' ); ?>
+
+<section class="sleep_cart">
+	<div class="sleep_cart_wrapper">
+		<div class="sleep_cart_left">
+			<div class="sleep_cart_login">
+				<?php
+				echo do_shortcode('[my_wc_login_form]');
+				?>
+			</div>
+			<?php
+
+				if ( ! is_user_logged_in() ) { ?>
+					<div class="sleep_cart_guest_purchase">
+						<h2>Purchase as guest</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commo Lorem ipsum </p>
+						<p class="sleep_guest_btn"><a href="<?php echo esc_url('/checkout'); ?>">Continue as a guest</a></p>
+					</div>
+				<?php }
+
+			 ?>
+		</div>
+
+		<div class="sleep_cart_right">
+			<div class="sleep_cart_item">
+			<?php
+				foreach ( WC()->cart->get_cart() as $cart_item_key => $cart_item ) { ?>
+					<div class="sleep_single_cart_item">
+					<?php
+					$_product   = apply_filters( 'woocommerce_cart_item_product', $cart_item['data'], $cart_item, $cart_item_key );
+					$product_id = apply_filters( 'woocommerce_cart_item_product_id', $cart_item['product_id'], $cart_item, $cart_item_key );
+
+					if ( $_product && $_product->exists() && $cart_item['quantity'] > 0 && apply_filters( 'woocommerce_cart_item_visible', true, $cart_item, $cart_item_key ) ) {
+						$product_permalink = apply_filters( 'woocommerce_cart_item_permalink', $_product->is_visible() ? $_product->get_permalink( $cart_item ) : '', $cart_item, $cart_item_key );
+						?>
+						<div class="single_cart_item_left">
+							<p class="sleep_product_name" data-title="<?php esc_attr_e( 'Product', 'woocommerce' ); ?>">
+								<?php
+								if ( ! $product_permalink ) {
+									echo wp_kses_post( apply_filters( 'woocommerce_cart_item_name', $_product->get_name(), $cart_item, $cart_item_key ) . '&nbsp;' );
+								} else {
+									echo wp_kses_post( apply_filters( 'woocommerce_cart_item_name', sprintf( '<a href="%s">%s</a>', esc_url( $product_permalink ), $_product->get_name() ), $cart_item, $cart_item_key ) );
+								}
+
+								do_action( 'woocommerce_after_cart_item_name', $cart_item, $cart_item_key );
+
+								// Meta data.
+								echo wc_get_formatted_cart_item_data( $cart_item ); // PHPCS: XSS ok.
+
+								// Backorder notification.
+								if ( $_product->backorders_require_notification() && $_product->is_on_backorder( $cart_item['quantity'] ) ) {
+									echo wp_kses_post( apply_filters( 'woocommerce_cart_item_backorder_notification', '<p class="backorder_notification">' . esc_html__( 'Available on backorder', 'woocommerce' ) . '</p>', $product_id ) );
+								}
+								?>
+							</p>
+
+							<p class="sleep_cart_price_wrapper">
+								<span class="sleep_cart_quantity">
+									<?php echo apply_filters( 'woocommerce_widget_cart_item_quantity', '<span class="quantity">' . sprintf( '%s &times; %s', $cart_item['quantity'], $product_price ) . '</span>', $cart_item, $cart_item_key ); ?>
+								</span>
+								<span class="sleep_cart_price">
+									<?php echo apply_filters( 'woocommerce_cart_item_price', WC()->cart->get_product_price( $_product ), $cart_item, $cart_item_key ); // PHPCS: XSS ok.
+								?>
+								</span>
+								
+							</p>
+							<p class="sleep_product_remove">
+								<?php
+									echo apply_filters( // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+										'woocommerce_cart_item_remove_link',
+										sprintf(
+											'<a href="%s" class="remove" aria-label="%s" data-product_id="%s" data-product_sku="%s">Remove</a>',
+											esc_url( wc_get_cart_remove_url( $cart_item_key ) ),
+											esc_html__( 'Remove this item', 'woocommerce' ),
+											esc_attr( $product_id ),
+											esc_attr( $_product->get_sku() )
+										),
+										$cart_item_key
+									);
+								?>
+							</p>
+						</div>
+						<div class="single_cart_item_right">
+							<p class="sleep_product_thumbnail">
+								<?php
+								$thumbnail = apply_filters( 'woocommerce_cart_item_thumbnail', $_product->get_image(), $cart_item, $cart_item_key );
+
+								if ( ! $product_permalink ) {
+									echo $thumbnail; // PHPCS: XSS ok.
+								} else {
+									printf( '<a href="%s">%s</a>', esc_url( $product_permalink ), $thumbnail ); // PHPCS: XSS ok.
+								}
+								?>
+							</p>
+						</div>
+
+						<?php
+					}
+					?>
+				</div>
+					<?php 
+				} ?>
+				<div class="sleep_product_subtotal">
+					<h3>Total</h3>
+					<span class="sleep_subtotal_amount">
+						<?php wc_cart_totals_subtotal_html(); ?>
+					</span>
+				</div>
+				<?php 
+				if ( is_user_logged_in() ) { ?>
+					<div class="proceed_checkout">
+						<?php do_action( 'woocommerce_proceed_to_checkout' ); ?>
+					</div>
+				<?php }
+			?>
+			</div>
+		</div>
+	</div>
+</section>
+
+<?php do_action( 'woocommerce_after_cart' ); ?>
